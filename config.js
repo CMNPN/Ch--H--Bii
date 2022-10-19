798b347baa1b7bc30dd625abb4ff1cb30850aaba
CMNPN <116182493+CMNPN@users.noreply.github.com>
2022-10-19 22:55:28 +0700

    Add files via upload
    Gift-20.10

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Chị Hà Bii';
+const giftUrl = 'Chị-Hà-Bii.png';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Chị Hà Bii';
+const contentCard = 'Chúc Mừng Ngày Quốc Tế Phụ Nữ 20/10 💕💕';
+
+const Image = 'Chị-Hà.jpg';
+const giftImage = 'bo-hoa-1-removebg.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
